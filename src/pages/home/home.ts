3d4b01bad59f838a8b6d615d0cbfd2f6705cdcda
Krunal Patel <kp25315@gmail.com>
2017-05-30 22:16:40 -0700

    added geolocation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
 
   }
 
+  ngOnInit(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log(resp.coords.latitude);
+      console.log(resp.coords.longitude);
+      console.log(resp.coords);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
 }
