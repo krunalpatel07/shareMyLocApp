54c74964c85a0778815f8cfb9241765e2c30d4ee
Krunal Patel <krunal.patel@csaa.com>
2017-05-31 15:29:26 -0700

    added maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Geolocation } from '@ionic-native/geolocation';
+import { AgmCoreModule } from '@agm/core';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
 
 @NgModule({
   declarations: [
@@ -15,6 +17,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBpZGFqnLcpDLyr0XNOUniCom0iap9cHZA'
+    }),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,7 @@ import { Geolocation } from '@ionic-native/geolocation';
   templateUrl: 'home.html'
 })
 export class HomePage implements OnInit{
+  userInfo: any = {};
 
   constructor(public navCtrl: NavController, private geolocation: Geolocation) {
 
@@ -14,6 +15,8 @@ export class HomePage implements OnInit{
 
   ngOnInit(){
     this.geolocation.getCurrentPosition().then((resp) => {
+      this.userInfo.latitude = resp.coords.latitude;
+      this.userInfo.longitude = resp.coords.longitude;
       console.log(resp.coords.latitude);
       console.log(resp.coords.longitude);
       console.log(resp.coords);
